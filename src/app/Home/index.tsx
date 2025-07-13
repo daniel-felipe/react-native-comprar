d129385291098fa99e7a628ab7df84ed4523050b
= <daniell.felipe.2017@gmail.com>
2025-07-13 12:08:56 -0300

    add Input component with styles and integrate into Home component

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,5 +1,6 @@
 import { Image, View } from 'react-native'
 import { Button } from '@/components/Button'
+import { Input } from '@/components/Input'
 import { styles } from './styles'
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
 		<View style={styles.container}>
 			<Image source={require('@/assets/logo.png')} style={styles.logo} />
 
+			<Input placeholder="O que você precisa comprar" />
 			<Button title="Entrar" />
 		</View>
 	)
