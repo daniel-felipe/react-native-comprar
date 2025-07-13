9e5a63940e816839c2609607d18f69c93056ab2f
= <daniell.felipe.2017@gmail.com>
2025-07-13 10:58:13 -0300

    add Button component with styles and integrate into Home component

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,11 +1,13 @@
 import { Image, View } from 'react-native'
-
+import { Button } from '@/components/Button'
 import { styles } from './styles'
 
 export default function Home() {
 	return (
 		<View style={styles.container}>
 			<Image source={require('@/assets/logo.png')} style={styles.logo} />
+
+			<Button title="Entrar" />
 		</View>
 	)
 }
